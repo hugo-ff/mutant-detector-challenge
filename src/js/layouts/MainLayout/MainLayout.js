82f0ab3f5b0894e4ce71efb1858ac78ca3e11626
Hugo <hugo.fernando.flotts@gmail.com>
2021-08-09 23:00:25 -0300

    feat(babel config, changelog file, js folder): aliases added to babel. changelog created. js folder
    module-resolver aliases were added to babel.config.json. changelog file for this branch created.
    MainLayout and Header file structures were created.

---
+++ src/js/layouts/MainLayout/MainLayout.js
@@ -0,0 +1,10 @@
+import Header from 'components/Header';
+import styles from './styles';
+
+const MainLayout = () => (
+	<styles.MainLayout>
+		<Header />
+	</styles.MainLayout>
+);
+
+export default MainLayout;
